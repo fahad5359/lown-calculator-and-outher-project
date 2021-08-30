f8d82ed93eae824564c9e1cc4a58243ce15700ce
fahad5359 <fabbgm5359@gmail.com>
2021-08-30 19:24:28 +0300

    simple audtion , trying to understand html and js

---
+++ calc.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function calcualalte(){
+  var v, vv, k;
+ v = parseInt(document.getElementById("onee").value);
+ vv = parseInt(document.getElementById("too").value);
+ 
+  k = v + vv;
+  document.getElementById("eq").value = k;
+}
+
+
