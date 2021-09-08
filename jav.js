97668230ab53f1fcf2903aded70f97a7662d3666
fahad5359 <fabbgm5359@gmail.com>
2021-09-08 19:05:28 +0300

    ..

---
+++ jav.js
@@ -0,0 +1,14 @@
+document.querySelector("#btn");
+
+
+function calcu(e) {
+  console.log('amk');
+  // الحين  سححب كل الانبت الموجودة as will be shown bellow
+  const amount=document.querySelector("#amount");
+  const inttrest=document.querySelector("#intrest");
+  const yearstopay=document.querySelector("#yrstpay");
+  const mounthlypaiment=document.querySelector("#mounth-p");
+  const totalpayment=document.querySelector("#total-p");
+  const totalintrest=document.querySelector("#total-i");
+
+}
